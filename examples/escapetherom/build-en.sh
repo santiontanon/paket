72b261ac817c76e3fc1f9d9ef27a5d2e3c6e692a
Santiago Ontañón <santi@Santiagos-MacBook-Pro.local>
2025-11-17 21:51:52 -0500

    added build files for all localizations of escape the rom and the key ep 1

---
+++ examples/escapetherom/build-en.sh
@@ -0,0 +1,2 @@
+# Author: Santiago Ontañón (2025)
+java -jar ../../target/PAKET.jar cpc/game.pak cpc en cpc/built/ -info -partial-localization -rooms-per-bank 2 -sfx-player ayfx -text-opt 2
---
+++ examples/thekey-ep1/build-en64.sh
@@ -0,0 +1,3 @@
+# Author: Santiago Ontañón (2025)
+java -jar ../../target/PAKET.jar cpc/game.pak cpc en64 cpc/built -info -rooms-per-bank 2 -sfx-player ayfx -partial-localization -text-opt 1 -object-opt 1 -room-opt 1 -tile-opt 1
+
